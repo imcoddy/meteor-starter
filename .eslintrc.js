9d3da7f29004088216f331cbd005b287d5b68270
imcoddy <imcoddy@gmail.com>
2018-11-28 17:38:53 +0900

    Update eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
-    "extends": "airbnb"
+	"extends": "airbnb",
+	"globals": {
+		"alert": false,
+		"describe": false,
+		"before": false,
+		"beforeEach": false,
+		"it": false,
+		"ReactiveVar": false,
+		"Template": false,
+	},
+	"rules": {
+		'import/extensions': ['off', 'never'],
+		"import/no-absolute-path": "off",
+		"import/no-unresolved": "off",
+	}
 };
