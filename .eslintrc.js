9441fb3b4d83648a35120bd76299ceda2f148d7f
imcoddy <imcoddy@gmail.com>
2018-12-03 10:01:03 +0900

    Update linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,4 @@
 module.exports = {
-	"extends": "airbnb",
-	"globals": {
-		"alert": false,
-		"describe": false,
-		"before": false,
-		"beforeEach": false,
-		"it": false,
-		"ReactiveVar": false,
-		"Template": false,
-	},
-	"rules": {
-		'import/extensions': ['off', 'never'],
-		"import/no-absolute-path": "off",
-		"import/no-unresolved": "off",
-	}
+	"plugins": ["meteor"],
+	"extends": ["plugin:meteor/recommended"],
 };
