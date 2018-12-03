3a1c8aaebd68a4f7797559bbc5220ad505d9a9ed
imcoddy <imcoddy@gmail.com>
2018-12-03 09:09:56 +0900

    Add paginator

--- imports/api/links/server/publications.js
+++ imports/api/links/server/publications.js
@@ -1,6 +1,14 @@
 // All links-related publications
 
-import { Meteor } from 'meteor/meteor';
-import { Links } from '../links.js';
+import {
+	Meteor
+} from 'meteor/meteor';
+import {
+	Links
+} from '../links.js';
+import {
+	publishPagination
+} from 'meteor/kurounin:pagination';
 
+publishPagination(Links);
 Meteor.publish('links.all', () => Links.find());
