71c54171f604b0fb65ba25527c74e4b3b97ccb5a
imcoddy <imcoddy@gmail.com>
2019-02-28 17:43:26 +0900

    Add about link

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,21 +1,29 @@
-import { FlowRouter } from 'meteor/kadira:flow-router';
-import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import {FlowRouter} from 'meteor/kadira:flow-router';
+import {BlazeLayout} from 'meteor/kadira:blaze-layout';
 
 // Import needed templates
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
+import '../../ui/pages/about/main';
 import '../../ui/pages/not-found/not-found.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
   name: 'App.home',
   action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
-  },
+    BlazeLayout.render('App_body', {main: 'App_home'});
+  }
+});
+
+FlowRouter.route('/about', {
+  name: 'App.about',
+  action() {
+    BlazeLayout.render('App_body', {main: 'App_about'});
+  }
 });
 
 FlowRouter.notFound = {
   action() {
-    BlazeLayout.render('App_body', { main: 'App_notFound' });
-  },
+    BlazeLayout.render('App_body', {main: 'App_notFound'});
+  }
 };
